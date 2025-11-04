eed01639523521184ddd103b7531d20199acb2b1
calogerosorce <calogerosorce00@hotmail.com>
2025-11-04 13:44:03 +0100

    Add new import header

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import logo from "../img/dc-logo.png"
+
+export default function Header() {
+
+
+    return (
+        <header>
+            <img width={70} src={logo} alt="Logo" />
+            <nav className="nav justify-content-center  ">
+                <a className="nav-link" href="#"><strong>CHARACTERS</strong></a>
+                <a className="nav-link" href="#"><strong>COMICS</strong></a>
+                <a className="nav-link" href="#"><strong>MOVIES</strong></a>
+                <a className="nav-link" href="#"><strong>TV</strong></a>
+                <a className="nav-link" href="#"><strong>GAMES</strong></a>
+                <a className="nav-link" href="#"><strong>COLLECTIBLES</strong></a>
+                <a className="nav-link" href="#"><strong>VIDEOS</strong></a>
+                <a className="nav-link" href="#"><strong>FANS</strong></a>
+                <a className="nav-link" href="#"><strong>NEWS</strong></a>
+                <a className="nav-link" href="#"><strong>SHOPS</strong></a>
+            </nav>
+        </header>
+    )
+} 
